a39d840d291b4a4c70d3dc2a01e54354c8a9673c
mizuhoiimori <tanaka.mizuho.com@gmail.com>
2021-10-13 10:56:55 +0000

    error debug

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about"
-  resource :books, only:[:new, :create, :index ,:show, :edit, :update, :destroy]
-  resource :users, only:[:new, :create, :index, :show, :edit, :update]
+  resources :books, only:[:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only:[:new, :create, :index, :show, :edit, :update]
 end
